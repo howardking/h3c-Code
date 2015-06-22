02c485aabe48642d872d7da9c48ffef08384799a
Howard King <tianlangroot@gmail.com>
2015-06-22 20:23:58 +0800

    增加了源码文件中的注释

--- regex_check_ip.c
+++ regex_check_ip.c
@@ -1,3 +1,12 @@
+/**
+ * FileName:    regex_check_ip.c
+ * Description: 使用正则表达式判断IP地址的正确性。如果正确，则交付到下层使用。
+ * Date:        2015-6-22
+ * Author:      王帅华
+ * Version:     V0.1
+ * */
+
+
 #include <sys/types.h>
 #include <regex.h>
 #include <stdio.h>
